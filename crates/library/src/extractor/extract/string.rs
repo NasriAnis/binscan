853ddef89d56514231aa020045f61ae87bdfa5d0
NasriAnis <nasrianis213@gmail.com>
2026-05-20 21:44:01 +0100

    implemented string parsing algorythm

---
+++ crates/library/src/extractor/extract/string.rs
@@ -0,0 +1,19 @@
+pub fn string_extraction(buffer: &Vec<u8>) -> Vec<String>
+{
+    // loop throught the buffer byte and store
+    // ASCII characteres into a temporary Vec
+    let mut temp: Vec<u8> = Vec::new();
+    let mut result: Vec<String> = Vec::new();
+    for byte in buffer {
+        if byte.is_ascii_graphic() || *byte == b' ' {
+            temp.push(*byte);
+        }
+        else {
+            if temp.len() >= 6 {
+                result.push(String::from_utf8_lossy(&temp).to_string())
+            }
+            temp.clear();
+        }
+    }
+   result
+}
