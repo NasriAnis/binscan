ecdd0925689e369659547aae123824749d4e84ad
NasriAnis <nasrianis213@gmail.com>
2026-05-22 17:30:10 +0100

    structure and modules implementation

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,7 +1,14 @@
 mod commands;
 use commands::{scan, report, extract, parsing};
 
-fn main() {
+use library::extractor::extract::extract as lib_extract;
+
+fn main()
+{
     // parse cmd input into struct
     let args = parsing::run();
+    println!("{:?}", args);
+
+    // test only case haha
+    lib_extract(args.source);
 }
