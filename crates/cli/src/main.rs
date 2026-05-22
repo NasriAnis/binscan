9167b913542c09422b9152e82c748d53c09fbdea
NasriAnis <nasrianis213@gmail.com>
2026-05-22 17:42:29 +0100

    deleted debugging purpose code

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,14 +1,9 @@
 mod commands;
 use commands::{scan, report, extract, parsing};
 
-use library::extractor::extract::extract as lib_extract;
-
 fn main()
 {
     // parse cmd input into struct
     let args = parsing::run();
     println!("{:?}", args);
-
-    // test only case haha
-    lib_extract(args.source);
 }
