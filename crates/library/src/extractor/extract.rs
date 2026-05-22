8c70d0bd4d31b4b9779e9554f377250ec9fb0613
NasriAnis <nasrianis213@gmail.com>
2026-05-22 17:28:54 +0100

    added the elf parser

---
+++ crates/library/src/extractor/extract.rs
@@ -0,0 +1,59 @@
+use std::fs;
+use goblin::{Object, error};
+
+use crate::{Extracted_modules,
+            extractor::extract::string::string_extraction,
+            extractor::extract::elf::{extract_elf_imports, extract_elf_symbols},
+            extractor::extract::pe::{extract_pe_imports, extract_pe_symbols}};
+
+mod string;
+mod elf;
+mod pe;
+
+pub fn extract(path_to_buf: String) ->  Result<Extracted_modules, error::Error>
+{
+    // Reads the entire contents of a file into a bytes vector.
+    let buffer;
+    match fs::read(path_to_buf) {
+        Ok(t) => buffer = t,
+        _ => {
+            return Err(error::Error::Malformed("Unsupported file type".to_string()))
+        }
+    };
+
+    let mut extracted_strings: Vec<String> = vec!["".to_string()];
+    let mut extracted_symbols: Vec<String> = vec!["".to_string()];
+    let mut extracted_imports: Vec<String> = vec!["".to_string()];
+
+    extracted_strings = string_extraction(&buffer);
+
+    // match binary type using goblin
+    match Object::parse(&buffer)?
+    {
+        Object::Elf(_) => {
+            extracted_symbols = extract_elf_symbols(&buffer);
+            extracted_imports = extract_elf_imports(&buffer);
+        },
+
+        Object::PE(_) => {
+            extracted_symbols = extract_pe_symbols(&buffer);
+            extracted_imports = extract_pe_imports(&buffer);
+        },
+
+        _ => {
+            return Err(error::Error::Malformed("Unsupported file type".to_string()))
+        }
+    }
+
+    // saving data into public structure
+    let extracted = Extracted_modules {
+        strings: extracted_strings,
+        symbols: extracted_symbols,
+        imports: extracted_imports,
+    };
+
+    println!("Extracted Strings : \n{:?}\n\n extracted symbols : \n{:?}\n\n Extracted imports : \n{:?}",
+                extracted.strings, extracted.symbols, extracted.imports);
+
+    Ok(extracted)
+}
