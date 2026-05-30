ccce1aa74902279ea5e4a2b6bc5f8eef5aad1e65
NasriAnis <nasrianis213@gmail.com>
2026-05-30 19:11:36 +0100

    cleaning + implemted file type

--- crates/library/src/lib.rs
+++ crates/library/src/lib.rs
@@ -1,11 +1,27 @@
-mod analyzer;
+pub mod analyzer;
 pub mod extractor;
 mod report;
 mod sbom;
 mod vuln_db;
 
+pub enum filetype {
+    PE,
+    ELF,
+    Other,
+}
+
 pub struct Extracted_modules {
+    pub file_type: filetype,
     pub strings: Vec<String>,
     pub symbols: Vec<String>,
     pub imports: Vec<String>,
+}
+
+pub struct Binary_data {
+    pub binary: String,
+    pub format: String,
+    pub compiler: String,
+    pub detected_components: Vec<String>,
+    pub bahvior_flags: Vec<String>,
+    pub build_info: Vec<String>,
 }
