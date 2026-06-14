031b5fca838f1ee535b663ea1e4b947f8f6b50a3
NasriAnis <nasrianis213@gmail.com>
2026-06-14 08:15:21 +0100

    Added extractor wrapper for CLI

---
+++ crates/cli/src/commands/extractor_cli.rs
@@ -0,0 +1,27 @@
+use core::panic;
+
+use library::{ExtractedModules, extractor::extract};
+use owo_colors::OwoColorize;
+use indicatif;
+
+use crate::commands::parsing::ParsedArgs;
+
+pub fn run(args: ParsedArgs) -> ExtractedModules
+{
+    let pb = indicatif::ProgressBar::new_spinner();
+    pb.set_message("Extracting data from binary...");
+    
+    let extraction_result: ExtractedModules;
+    match extract::run(args.source) {
+        Ok(t) => {
+            extraction_result = t;
+            pb.finish();
+            extraction_result
+        },
+        Err(e) => {
+            pb.finish();
+            println!("{}: {}", "Error in extraction".red().bold(), e.red().bold());
+            panic!()
+        }
+    }
+}
--- crates/cli/src/commands/mod.rs
+++ crates/cli/src/commands/mod.rs
@@ -1,1 +1,2 @@
 pub mod parsing;
+pub mod extractor_cli;
