dd3562b7dbd4b1f43b72d707391f0a41f3824ac6
NasriAnis <nasrianis213@gmail.com>
2026-06-14 08:15:50 +0100

    Added new parser argument

--- crates/cli/src/commands/parsing.rs
+++ crates/cli/src/commands/parsing.rs
@@ -7,6 +7,15 @@ pub struct ParsedArgs {
     #[arg(short = 's', long, help = "Path to the source file")]
     pub source: String,
 
+    #[arg(short = 'A', long, help = "Check via API")]
+    pub api: bool,
+
+    #[arg(short = 'S', long, help = "Check for severity levels.", requires="api")]
+    pub severity: Option<Severity>,
+
+    #[arg(short = 'e', long, help = "Ecosystem, default: Debian13", requires="api")]
+    pub ecosystem: Option<String>,
+
     // saving report in the file specified after -r (optional)
     #[arg(short = 'r', long, help = "Path to save the report")]
     pub report: Option<String>,
@@ -14,4 +23,13 @@ pub struct ParsedArgs {
 
 pub fn run() -> ParsedArgs {
     ParsedArgs::parse()
+}
+
+#[derive(Debug, Clone, clap::ValueEnum)]
+pub enum Severity{
+    NotAssigned,
+    Unimportant,
+    Low,
+    Medium,
+    High,
 }
